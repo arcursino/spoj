f4da60ce89764a1bfd67be412ff4264bd85cec63
arcursino <arcursino@gmail.com>
2019-04-15 22:18:29 -0300

    resolução do desafio f91

---
+++ f91/f91.c
@@ -0,0 +1,22 @@
+//tag: recursivo, begginner, function, f91.
+
+#include <stdio.h>
+
+int f91(int n){
+    if (n >= 101){
+       return (n - 10);
+    } else {
+        return f91 (f91 (n + 11));
+    }
+}
+
+int main(){
+    int n, result;
+
+    n = 1;
+
+    while (scanf("%d", &n) && n  != 0){
+        result = f91(n);
+        printf("f91(%d) = %d\n", n, result);
+    }
+}
