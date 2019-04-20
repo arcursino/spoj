f50f6a924030571de6c5bba5a83d8a7ad17beb6f
arcursino <arcursino@gmail.com>
2019-04-20 17:55:39 -0300

    corrige lint

--- f91/f91.c
+++ f91/f91.c
@@ -1,21 +1,21 @@
-//tag: recursivo, begginner, function, f91.
+// tag: recursivo, begginner, function, f91.
 
 #include <stdio.h>
 
-int f91(int n){
-    if (n >= 101){
+int f91(int n) {
+    if (n >= 101) {
        return (n - 10);
     } else {
         return f91 (f91 (n + 11));
     }
 }
 
-int main(){
+int main() {
     int n, result;
 
     n = 1;
 
-    while (scanf("%d", &n) && n  != 0){
+    while (scanf("%d", &n) && n  != 0) {
         result = f91(n);
         printf("f91(%d) = %d\n", n, result);
     }
