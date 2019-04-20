a4d88a4bfd82de7f77f0cdcf1066a970a219d4bc
arcursino <arcursino@gmail.com>
2019-04-20 17:42:17 -0300

    adiciona cpplint e run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,59 @@
+ #!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ "${1:-1}" == '-fail-on-warnings' ]]; then
+    warning_flags='-Werror'
+else
+    warning_flags='-Wall'
+fi
+
+function run_c {
+    gcc $warning_flags ./*.c -lm
+
+    if [[ -f in.txt ]]; then
+        ./a.out < in.txt > result.txt
+    else
+        ./a.out > result.txt
+    fi
+
+    rm a.out
+}
+
+function run_python {
+    if [[ -f in.txt ]]; then
+        python ./*.py < in.txt > result.txt
+    else
+        python ./*.py > result.txt
+    fi
+}
+
+function run_and_check {
+    if [[ "$(find . -name '*.c' | wc -l)" -eq 1 ]]; then
+        run_c
+        diff out.txt result.txt
+    fi
+
+    if [[ "$(find . -name '*.py' | wc -l)" -eq 1 ]]; then
+        run_python
+        diff out.txt result.txt
+    fi
+
+    rm result.txt
+}
+
+function run_all {
+    shopt -s globstar nullglob # needed for ./**
+
+    for file in ./**/problem.txt ; do
+        folder="$(dirname "$file")"
+        cd "$folder" || exit 1
+        echo "$folder"
+        if [[ ! -f 'WRONG' ]]; then
+            run_and_check
+        fi
+        cd - > /dev/null || exit 1
+    done
+}
+
+run_all
