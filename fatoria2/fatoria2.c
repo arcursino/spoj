6b26166b84ecd54e3572328fd19b7424725fe962
arcursino <arcursino@gmail.com>
2019-04-20 17:20:41 -0300

    adiciona a resolução fatoria2

---
+++ fatoria2/fatoria2.c
@@ -0,0 +1,27 @@
+//tags: recursivo, fatorial, iniciante.
+#include <stdio.h>
+
+int fat(int n);
+
+int main(){
+    int n;
+
+    n = 1;
+
+    while (scanf("%d", &n) >= 0 && n <= 12){
+        printf("%d\n", fat(n));
+        break;
+    }
+}
+
+int fat(int n){
+        if (n == 0){
+            return n + 1;
+
+        }else{
+            return n * fat(n-1);
+        }
+}
+
+
+
